1aeac5aeed78b42b580529b1c5d97c744fa16e3e
DaniilKopylov <38dunn@mail.ru>
2019-03-07 23:16:52 +0800

    Перенес функции в функции, но наверное лучше было использовать глобальные переменные

--- ДЗ_4/python-1-lesson4-hard.py
+++ ДЗ_4/python-1-lesson4-hard.py
@@ -14,39 +14,38 @@ person3 = {'card': 4276123465440002, 'pin': 9092, 'money': 300.90}
 bank = [person1, person2, person3]
 
 
-def get_person_by_card(card_number):
-    for person in bank:
-        if person['card'] == card_number:
-            return person
 
-
-def is_pin_valid(person, pin_code):
-    if person['pin'] == pin_code:
-        return True
-    return False
-
-
-def check_account(person):
-    return round(person['money'], 2)
-
-
-def withdraw_money(person, money):
-    if person['money'] - money == 0:
-        person['money'] -= money
-        return 'Вы сняли {} рублей.'.format(money)
-    else:
-        return 'На вашем счету недостаточно средств!'
-
-
-def process_user_choice(choice, person):
-    if choice == '1':
-        print(check_account(person))
-    elif choice == '2':
-        count = float(input('Сумма к снятию:'))
-        print(withdraw_money(person, count))
 
 
 def start():
+    def get_person_by_card(card_number):
+        for person in bank:
+            if person['card'] == card_number:
+                return person
+
+    def is_pin_valid(person, pin_code):
+        if person['pin'] == pin_code:
+            return True
+        return False
+
+    def process_user_choice(choice, person):
+
+
+        def check_account(person):
+            return round(person['money'], 2)
+
+        def withdraw_money(person, money):
+            if person['money'] - money == 0:
+                person['money'] -= money
+                return 'Вы сняли {} рублей.'.format(money)
+            else:
+                return 'На вашем счету недостаточно средств!'
+
+        if choice == '1':
+            return check_account(person)
+        elif choice == '2':
+            count = float(input('Сумма к снятию:'))
+            return withdraw_money(person, count)
     card_number, pin_code = input('Введите номер карты и пин код через пробел:').split()
 
     card_number = int(card_number)
@@ -62,7 +61,7 @@ def start():
                                'Ваш выбор:'))
             if choice == 3:
                 break
-            process_user_choice(choice, person)
+            print(process_user_choice(choice, person))
     else:
         print('Номер карты или пин код введены не верно!')
 
