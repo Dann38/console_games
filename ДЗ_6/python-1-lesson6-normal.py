495c8fbcac689e5353febbc657e93e59a757ba9d
DaniilKopylov <38dunn@mail.ru>
2019-03-14 21:06:32 +0800

    Добавил класс черепах, сделал функцию атаки

--- ДЗ_6/python-1-lesson6-normal.py
+++ ДЗ_6/python-1-lesson6-normal.py
@@ -10,3 +10,36 @@
 # Функция подсчета урона должна быть инкапсулирована
 # Вам надо описать игровой цикл так же через класс.
 # Создайте экземпляры классов, проведите бой. Кто будет атаковать первым оставляю на ваше усмотрение.
+"""
+Я не помню когда мы писали игру, но я придумал свою, надеюсь задание выполнино верно
+"""
+import random
+class Turtel:
+    def __init__(self):
+        self.hp = 1000
+        self.attack_arm_hp = 50
+
+    def attack_arm(self, attack_, player):
+        fortun = int(random.randint(0,100))
+        if fortun >67:
+            player.hp -= attack_
+
+
+class Leonarda(Turtel):
+    def __init__(self):
+        Turtel.__init__(self)
+
+# class Donatella(Turtel):
+# class Micilangelo(Turtel):
+# class Rafaaly(Turtel):
+
+player1 = Leonarda()
+player2 = Leonarda()
+
+print(player1.hp)
+print(player2.hp)
+
+player1.attack_arm(player1.attack_arm_hp, player2)
+
+print(player1.hp)
+print(player2.hp)
